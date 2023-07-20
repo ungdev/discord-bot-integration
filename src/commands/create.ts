e96aa47d385997a2edabfff7c8111e06aa6f33fb
chrischidiac <christopher.chidiac@utt.fr>
2023-07-20 09:19:58 +0200

    Test bot create

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -30,7 +30,9 @@ async function create() {
     );
 
     await Promise.all(
+        log(`Test 1`);
         global.data.teams.map(async (team: any) => {
+            log(`Test 2`);
             if (team.name !== undefined && team.name !== null && team.name !== '') {
                 log(`Team ${team.name}`);
                 await addRole(team.name);
